99b3393846bcb43c1db008bc8dfc68262e53e9da
2808krish <kvpatel2808@gmail.com>
2026-08-05 18:18:15 +0530

    Learned Output Parsers : String, JSON, Pydantic.

--- app.py
+++ app.py
@@ -2,18 +2,47 @@ from dotenv import load_dotenv
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel
 
 load_dotenv()
+
+class langChain(BaseModel):
+    country : str
+    capital : str
+    language : str
+    
+parser = PydanticOutputParser(pydantic_object=langChain)
 
 llm = ChatGoogleGenerativeAI(
     model = "gemini-3.6-flash"
 )
 
-template = PromptTemplate.from_template(
-    "What is the capital of {country}?, Answer in {language}"
+template = PromptTemplate(
+    template="""
+Answer the following question.
+
+{format_instructions}
+
+Country: {country}
+Language: {language}
+""",
+    input_variables = ["country", "language"],
+    partial_variables =
+    {
+        "format_instructions": parser.get_format_instructions()
+    }
 )
-prompt = template.format(country="Japan", language= "Hindi")
 
-response= llm.invoke(prompt)
+chain = template | llm | parser
 
-print(response.content[0]["text"])
+response = chain.invoke(
+    {
+        "country":"Spain",
+        "language":"Spanish"
+    }
+)
+
+print(response)
