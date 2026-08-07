890b596b4adc083fdc4828b7676bd7cc9f88bb0c
2808krish <kvpatel2808@gmail.com>
2026-08-07 15:21:04 +0530

    Learned ChatPromptTemplate

--- app.py
+++ app.py
@@ -1,56 +1,29 @@
 from dotenv import load_dotenv
-import os
+
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import PydanticOutputParser
-from pydantic import BaseModel
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.runnables import RunnableParallel
-from langchain_core.runnables import RunnableLambda
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-class langChain(BaseModel):
-    country : str
-    capital : str
-    language : str
-    
-parser = PydanticOutputParser(pydantic_object=langChain)
-
 llm = ChatGoogleGenerativeAI(
-    model = "gemini-3.6-flash"
+    model="gemini-3.6-flash"
 )
 
-template = PromptTemplate(
-    template="""
-Answer the following question.
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an expert travel guide."),
+        ("human", "Tell me about {country}.")
+    ]
+)
 
-{format_instructions}
+parser = StrOutputParser()
 
-Country: {country}
-Language: {language}
-""",
-    input_variables = ["country", "language"],
-    partial_variables =
+chain = prompt | llm | parser
+
+response = chain.invoke(
     {
-        "format_instructions": parser.get_format_instructions()
-    }
-)
-
-capital_chain = template | llm | parser
-
-def extract_country(data):
-    return data["country"]
-
-main_chain = RunnableParallel(
-    country=RunnableLambda(extract_country),
-    capital=capital_chain
-)
-
-response = main_chain.invoke(
-    {
-        "country":"Spain",
-        "language":"Spanish"
+        "country": "Japan"
     }
 )
 
