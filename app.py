40903815a88c0cfb72ccb451dbfbf6d3b6106976
2808krish <kvpatel2808@gmail.com>
2026-08-07 15:24:25 +0530

    Learned MessagePlaceholder

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
@@ -12,8 +13,9 @@ llm = ChatGoogleGenerativeAI(
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are an expert travel guide."),
-        ("human", "Tell me about {country}.")
+        ("system", "You are a helpful assistant."),
+        MessagesPlaceholder("history"),
+        ("human", "{question}")
     ]
 )
 
@@ -21,9 +23,17 @@ parser = StrOutputParser()
 
 chain = prompt | llm | parser
 
+history = [
+    HumanMessage(content="Hi"),
+    AIMessage(content="Hello!"),
+    HumanMessage(content="My name is Walter Hartwell White"),
+    AIMessage(content="Nice to meet you!")
+]
+
 response = chain.invoke(
     {
-        "country": "Japan"
+        "history": history,
+        "question": "What is my name?"
     }
 )
 
